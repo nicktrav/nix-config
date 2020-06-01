7819db5b2e4b785515a2072f366ed3dccbaa4f56
Nick Travers <n.e.travers@gmail.com>
2020-05-31 21:32:58 -0700

    Update curl
    Signed-off-by: Nick Travers <n.e.travers@gmail.com>

--- programs/curl/default.nix
+++ programs/curl/default.nix
@@ -3,14 +3,14 @@
 with pkgs;
 
 curl.overrideAttrs ( old: rec {
-  name = "curl-7.69.1";
+  name = "curl-7.70.0";
 
   src = fetchurl {
     urls = [
       "https://curl.haxx.se/download/${name}.tar.bz2"
       "https://github.com/curl/curl/releases/download/${lib.replaceStrings ["."] ["_"] name}/${name}.tar.bz2"
     ];
-    sha256 = "1s2ddjjif1wkp69vx25nzxklhimgqzaazfzliyl6mpvsa2yybx9g";
+    sha256 = "1l19b2xmzwjl2fqlbv46kwlz1823miaxczyx2a5lz8k7mmigw2x5";
   };
 
   # Compile against boringssl
