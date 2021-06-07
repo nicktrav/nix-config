39d5b221aa985bc09973c26a43b6ca91121229f3
Nick Travers <n.e.travers@gmail.com>
2021-06-07 09:03:24 -0700

    Add jetbrains

--- home/default.nix
+++ home/default.nix
@@ -1,11 +1,12 @@
 user:
-{ ...}: {
+{ ... }: {
   imports = [
     ./alacritty.nix
     ./bash
     ./firefox.nix
     (import ./git.nix user)
     ./htop.nix
+    ./jetbrains.nix
     ./programs.nix
     ./tmux
     ./vim
