a5f0576d3afc89602492d63b581aa5951be85870
Nick Travers <n.e.travers@gmail.com>
2021-06-11 07:45:21 -0700

    Htop tweaks

--- home/htop.nix
+++ home/htop.nix
@@ -2,7 +2,10 @@
   programs.htop = {
     enable = true;
     settings = {
+      hide_kernel_treads = true;
       hide_treads = true;
+      hide_userland_threads = true;
+      highlight_base_name = true;
       show_program_paths = false;
       tree_view = true;
     };
