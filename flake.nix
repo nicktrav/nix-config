4d9838d218fa65fac87fb26b1dd0a6d51ba82356
Nick Travers <n.e.travers@gmail.com>
2021-06-12 20:55:28 -0700

    Use stable and unstable packages

--- flake.nix
+++ flake.nix
@@ -3,6 +3,9 @@
     nixpkgs = {
       url = "github:nixos/nixpkgs/nixos-21.05";
     };
+    nixpkgs-unstable = {
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
