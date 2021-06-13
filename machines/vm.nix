de3bd25e5a24a5e9feca48587438056d2de8ef4e
Nick Travers <n.e.travers@gmail.com>
2021-06-12 20:55:28 -0700

    WIP - use stable and unstable packages

--- machines/vm.nix
+++ machines/vm.nix
@@ -1,16 +1,23 @@
 inputs: inputs.nixpkgs.lib.nixosSystem {
   system = "x86_64-linux";
-  modules = [
-    { system.stateVersion = "21.05"; }
-
-    inputs.home-manager.nixosModules.home-manager
-
-    {
-      imports = [
-        ./vm
-        ../profiles/desktop.nix
-        ../users/nickt.nix
-      ];
-    }
-  ];
+  modules =
+    let
+      # Allow for use of both stable and unstable packages.
+      # See: https://github.com/nix-community/home-manager/issues/1538
+      defaults = { pkgs, ... }: {
+        _module.args.pkgs-unstable =
+          import inputs.nixpkgs-unstable { inherit (pkgs.stdenv.targetPlatform) system; };
+      };
+    in [
+      defaults
+      { system.stateVersion = "21.05"; }
+      inputs.home-manager.nixosModules.home-manager
+      {
+        imports = [
+          (import ./vm inputs)
+          ../profiles/desktop.nix
+          ../users/nickt.nix
+        ];
+      }
+    ];
 }
