71b833decc65a0b4711795f4e30943d43ecfaa95
Nick Travers <n.e.travers@gmail.com>
2021-10-28 19:11:18 -0700

    Add .gitconfig

--- pkgs/git/default.nix
+++ pkgs/git/default.nix
@@ -1,15 +1,26 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 let
   git-tidy-up = pkgs.writeShellScriptBin "git-tidy-up" (builtins.readFile ./git-tidy-up );
 
 in {
-
   programs.git = {
     enable = true;
+    includes = [
+      { path = "~/.config/git/.gitconfig"; }
+    ];
+    ignores = [
+      "*.swn"
+      "*.swo"
+      "*.swp"
+      ".idea/"
+      ".ijwb/"
+    ];
   };
 
   home.packages = [
     git-tidy-up
   ];
+
+  xdg.configFile."git/.gitconfig".source = ./.gitconfig;
 }
