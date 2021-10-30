0e2216cffa405ae8e98d7b5fded03a61d56e3a13
Nick Travers <n.e.travers@gmail.com>
2021-10-30 15:42:49 -0700

    Deps for cdb

--- users/nickt/home-manager.nix
+++ users/nickt/home-manager.nix
@@ -13,30 +13,47 @@
   ];
 
   home.packages = (with pkgs; [
+    autoconf
+    binutils
     certigo
+    cmake
     curl
     dig
+    file
     firefox
+    gcc
+    gnumake
+    openjdk
     jq
     kubectx
     kubectl
+    libedit
     lsof
+    m4
+    ncurses
     nix-prefetch-git
+    nix-index
+    nodejs
     opensc
+    patchelf
     ripgrep
     shellcheck
     ssh-agents
+    stdenv.cc.cc.lib
     tree
     unzip
     usbutils
     wget
     whois
+    yacc
     yubico-piv-tool
     yubikey-manager
     yubioath-desktop
   ]) ++ (with pkgs-unstable; [
     _1password
     _1password-gui
+    bazel_4
+    bazelisk
     go
     go-tools
     google-chrome
