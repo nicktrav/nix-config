18e39e1500cbc807e1ac7a15d83a5acc25e6a5f7
Nick Travers <n.e.travers@gmail.com>
2021-10-31 13:29:01 -0700

    Rofi

--- pkgs/i3/default.nix
+++ pkgs/i3/default.nix
@@ -1,5 +1,10 @@
 { config, lib, pkgs, ... }:
 
 {
+  home.packages = with pkgs; [
+    rofi
+  ];
+
   xdg.configFile."i3/config".text = builtins.readFile ./i3;
+  xdg.configFile."rofi/config.rasi".text = builtins.readFile ./rofi;
 }
