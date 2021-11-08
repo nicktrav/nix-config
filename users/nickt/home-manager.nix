ee68674f8ff2eb60baceb640dd3ed563068af864
Nick Travers <n.e.travers@gmail.com>
2021-11-07 18:38:38 -0800

    Remove unecessary development deps

--- users/nickt/home-manager.nix
+++ users/nickt/home-manager.nix
@@ -19,51 +19,29 @@
   xdg.enable = true;
 
   home.packages = (with pkgs; [
-    autoconf
     binutils
     certigo
-    cmake
     curl
     dig
     file
-    firefox
-    gcc
-    gnumake
-    openjdk
     jq
-    killall
-    kubectx
-    kubectl
-    libedit
     lsof
-    m4
-    ncurses
     nix-prefetch-git
-    nix-index
-    nodejs
     opensc
-    patchelf
     ripgrep
     shellcheck
     ssh-agents
-    stdenv.cc.cc.lib
     tree
     unzip
     usbutils
-    wget
     whois
-    yacc
     yubico-piv-tool
     yubikey-manager
     yubioath-desktop
   ]) ++ (with pkgs-unstable; [
     _1password
     _1password-gui
-    bazel_4
-    bazelisk
     go
-    go-tools
-    golint
     google-chrome
   ]);
 }
