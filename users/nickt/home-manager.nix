c916da05f9825d60d43387b4f1c8fe3f5dcdc96e
Nick Travers <n.e.travers@gmail.com>
2021-11-10 20:17:12 -0800

    Add dict

--- users/nickt/home-manager.nix
+++ users/nickt/home-manager.nix
@@ -38,6 +38,7 @@ in {
   imports = [
     ./../../pkgs/alacritty.nix
     ./../../pkgs/bash
+    ./../../pkgs/dict.nix
     ./../../pkgs/direnv.nix
     (import ./../../pkgs/chrome { inherit pkgs pkgs-unstable; })
     ./../../pkgs/fzf.nix
