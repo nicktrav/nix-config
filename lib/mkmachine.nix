d67fe0618e8670af1b37b61474ed711dc0f3c154
Nick Travers <n.e.travers@gmail.com>
2022-06-04 15:57:22 -0700

    *: upgrade to 22.05

--- lib/mkmachine.nix
+++ lib/mkmachine.nix
@@ -5,32 +5,37 @@ name: { nixpkgs, nixpkgs-unstable, home-manager, nixos-hardware, system, user }:
 let
   pkgs = nixpkgs;
   lib = pkgs.lib;
+  hardware = (./.. + "/hardware/${name}.nix");
+  machine = (./.. + "/machines/${name}.nix");
 
 in
 nixpkgs.lib.nixosSystem rec {
   inherit system;
 
-  modules = let
-    # Allow for use of both stable and unstable packages.
-    # See: https://github.com/nix-community/home-manager/issues/1538
-    defaults = { pkgs, ... }: {
-      _module.args.nixpkgs-unstable =
-        import nixpkgs-unstable {
-          inherit (pkgs.stdenv.targetPlatform) system;
-          # Allow unfree packages in unstable.
-          # See: https://discourse.nixos.org/t/only-one-nixpkgs-in-a-flake-input-can-allow-unfree/8866
-          config.allowUnfree = true;
-        };
+  modules =
+    let
+      # Allow for use of both stable and unstable packages.
+      # See: https://github.com/nix-community/home-manager/issues/1538
+      defaults = { pkgs, ... }: {
+        _module.args.nixpkgs-unstable =
+          import nixpkgs-unstable {
+            inherit (pkgs.stdenv.targetPlatform) system;
+            # Allow unfree packages in unstable.
+            # See: https://discourse.nixos.org/t/only-one-nixpkgs-in-a-flake-input-can-allow-unfree/8866
+            config.allowUnfree = true;
+          };
       };
-  in [
-    defaults
-    home-manager.nixosModules.home-manager
-    (import ../hardware/${name}.nix { inherit lib pkgs nixos-hardware; })
-    ../machines/${name}.nix
-    ../users/nickt/nixos.nix
-  ];
-
-  extraArgs = {
-    currentSystem = system;
-  };
+    in
+    [
+      defaults
+      home-manager.nixosModules.home-manager
+      (import hardware { inherit lib pkgs nixos-hardware; })
+      machine
+      ../users/nickt/nixos.nix
+      {
+        config._module.args = {
+          currentSystem = system;
+        };
+      }
+    ];
 }
